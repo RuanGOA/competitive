94fb58d3c27c0f16f616136dd10857fa82771876
Ruan Gomes <49241164+RuanGOA@users.noreply.github.com>
2019-05-02 19:48:58 -0300

    Create unionFind.cpp

---
+++ Algorithms/Graphs/unionFind.cpp
@@ -0,0 +1,18 @@
+int find (int a){
+  if(pai[a] == a) return a;
+  return pai[a] = find(pai[a]);
+}
+
+void union(int a, int b){
+  pai_a = find(a);
+  pai_b = find(b);
+  
+  if(pai_a == pai_b]) return;
+  if(rank[pai_a] > rank[pai_b]){
+    rank[pai_a] += rank[pai_b];
+    pai[pai_b] = pai_a;
+  } else {
+    rank[pai_b] += rank[pai_a];
+    pai[pai_a] = pai_b;
+  }
+}
