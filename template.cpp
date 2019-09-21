7eb60157ab834bbd2e2535c895aee860e5473385
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2019-09-20 23:05:59 -0300

    template 0.0.1

---
+++ template.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define MAXS 100000007
+#define MAX 2147483647
+#define MOD 1000000007
+#define mset(x,y) memset(&x, (y), sizeof(x))
+#define mp(a, b) make_pair(a, b)
+#define IN freopen("input.cpp", sdtin, "r");
+#define OU freopen("output.cpp", stdout, "w");
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef queue<int> qi;
+typedef priority_queue<int, vector<int>, greater<int>> pqc;
+
+int arr[MAXS];
+
+int main() {
+   //IN OU
+   //cout << (unsigned long long) __builtin_inf() << endl;
+   
+   return 0;
+}
