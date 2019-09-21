7a38e34174baffef8b46a349eaabfaaf99e7a272
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2019-09-21 01:32:43 -0300

    segtree nogambi

--- Algorithms/DataStructures/SegTree.cpp
+++ Algorithms/DataStructures/SegTree.cpp
@@ -1,9 +1,12 @@
 #include <bits/stdc++.h>
-#define INF 1000000007
-#define MAXS 100007
-#define IN freopen("input.txt", "r", stdin);
-#define OU freopen("output.txt", "w", stdout);
+
+#define MAXS 100000007
+#define MAX 2147483647
+#define MOD 1000000007
+#define mset(x,y) memset(&x, (y), sizeof(x))
 #define mp(a, b) make_pair(a, b)
+#define IN freopen("input.cpp", sdtin, "r");
+#define OU freopen("output.cpp", stdout, "w");
 
 using namespace std;
 
@@ -12,72 +15,70 @@ typedef unsigned long long ull;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 typedef queue<int> qi;
-typedef queue<pii> qpi;
+typedef priority_queue<int, vector<int>, greater<int>> pqc;
 
-
-int n, q, lib[MAXS], in, to;
-ull segTree[MAXS * 4];
-
-ull query(int id, int left, int right, int x, int y) {
-    if(x <= left && right <= y) {
-        return segTree[id];
-    } else if(x > right || y < left) {
-        return 0;
-    } else {
-        int m = (left + right)/2;
-        return query(id * 2, left, m, x, y) + 
-        query(id * 2 + 1, m + 1, right, x, y);
-    }
-}
+int arr[MAXS];
+int st[MAXS];
 
 void build(int id, int left, int right) {
-    if(left == right) {
-        segTree[id] = lib[left];
-    } else {
-        int m = (left + right)/2;
-        build(id * 2, left, m);
-        build(id * 2 + 1, m + 1, right);
+   if(left == right) {
+      st[id] = arr[left];
+   } else {
+      int mid = (left + right)/2;
 
-        segTree[id] = segTree[id * 2] + segTree[id * 2 + 1];
-    }
+      build(id * 2, left, mid);
+      build(id * 2 + 1, mid + 1, right);
+
+      st[id] = st[id * 2 + 1] +  st[id * 2];
+   }
 }
 
-void update(int id, int left, int right, int pos, int val) {
-    if(left == right) {
-        segTree[id] = val;
-    } else {
-        int m = (left + right)/2;
-        if(pos <= m) {
-            update(id * 2, left, m, pos, val);
-        } else {
-            update(id * 2 + 1, m + 1, right, pos, val);
-        }
+void update(int id, int left, int right, int pos, int value) {
+   if(left == right) {
+      st[id] = value;
+   } else {
+      int mid = (left + right)/2;
+      if(pos <= mid) {
+         update(id * 2, left, mid, pos, value);
+      } else update(id * 2 + 1, mid + 1, right, pos, value);
+      st[id] = st[id * 2] + st[id * 2 + 1];
+   }
 
-        segTree[id] = segTree[id * 2] + segTree[id * 2 + 1];
-    }
+}
+
+int query(int id, int left, int right, int x, int y) {
+   if(x <= left && right <= y) return st[id];
+   else if(x > right || y < left) return 0;
+   else {
+      int mid = (left + right)/2;
+      return query(id * 2, left, mid, x, y) + query(id * 2 + 1, mid + 1, right, x, y);
+   }
 }
 
 int main() {
-    //IN OU
-    cin >> n;
-    for(int i = 1; i <= n; i++) {
-        scanf("%d", &lib[i]);
-    }
+   ios::sync_with_stdio(false);cin.tie(0);
+   //IN OU
+   //cout << (unsigned long long) __builtin_inf() << endl;
+   int n; cin >> n;
 
-    build(1, 1, n);
+   for(int i = 1; i <= n; i++) {
+      scanf("%d", &arr[i]);
+   }
 
-    cin >> q;
-    q *= 2;
-    char var;
-    for(int i = 0; i < q; i++) {
-        scanf("%c %d %d", &var, &in, &to);
+   build(1, 1, n);
 
-        if(var == 'U') {
-            update(1, 1, n, in, to);
-        } else if(var == 'Q') {
-            cout << query(1, 1, n, in, to) << endl;
-        }
-    }
+   int q; cin >> q;
+   int x, y; char var;
+   for(int i = 0; i < q; i++) {
+      cin >> var;
+      if(var == 'Q') {
+         cin >> x >> y;
+         cout << query(1, 1, n, x, y) << endl;
+      } else {
+         cin >> x >> y;
+         update(1, 1, n, x, y);
+      }
+   }
 
-    return 0;
+   return 0;
 }
